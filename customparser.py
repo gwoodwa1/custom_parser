b3fcd2adb3bc64e8c31d76ec51fc048fc0a59d6d
Gary Woodward <63735312+gwoodwa1@users.noreply.github.com>
2021-04-16 08:51:26 +0100

    Add files via upload

---
+++ customparser.py
@@ -0,0 +1,77 @@
+import re
+import yaml
+
+# =============================================#
+#                                              #
+#  Bespoke Parsers returning structured data : #
+#                                              #
+#  - Python Dictionary                         #
+#  - YAML File                                 #
+#                                              #
+#                                              #
+# =============================================#
+
+
+class CustomParsers:
+    
+    def showservdatabase(self, output=None, device_name=None, yaml_out=None):
+    
+        result = output
+
+        self.output_dict = {}
+
+        # 100:1:31000000-0000-0000-0000-520000000000 Connected *      1       392
+
+        pat1 = re.compile(
+            r"(?P<service>\d+):(?P<sub_service>\d+):(?P<instance>\S+)\s+(?P<trust>\w+)\s+(?P<domain>\S+)\s+(?P<owner>\d+)\s+(?P<size>\d+)"
+        )
+
+        index = 0
+
+        for line in result.splitlines():
+
+            if line:
+                line = line.strip()
+
+            else:
+                continue
+
+            match_pattern = pat1.match(line)
+
+            if match_pattern:
+                index = index + 1
+                group = match_pattern.groupdict()
+                
+                temp_dict = (
+                    self.output_dict.setdefault("hostname", {})
+                    .setdefault(device_name, {})
+                    .setdefault("index", {})
+                    .setdefault(index, {})
+                )
+                if group['service'] and group['service'].isdigit():
+                    temp_dict.update({"service_id": int(group["service"])})
+                if group['sub_service'] and group['service'].isdigit():
+                    temp_dict.update({"sub_service": int(group["sub_service"])})
+                
+                
+                temp_dict.update({"instance": group["instance"]})
+                temp_dict.update({"trust": group["trust"]})
+                temp_dict.update({"domain": group["domain"]})
+                if group['owner'] and group['owner'].isdigit():
+                    temp_dict.update({"owner": int(group["owner"])})
+                if group['size'] and group['size'].isdigit():
+                   temp_dict.update({"size": int(group["size"])})
+        import pdb; pdb.set_trace()
+        
+        if yaml_out:
+            
+            CustomParsers().yamlwriter(yaml_out=yaml_out, input_dict=self.output_dict)
+
+        return self.output_dict
+
+    def yamlwriter(self, yaml_out=None, input_dict=None):
+
+        with open(f'{yaml_out}', "w") as file:
+            
+            yaml.dump(input_dict, file, allow_unicode=True)
+        
---
+++ show_service_data.py
@@ -0,0 +1,23 @@
+from customparser import CustomParsers
+import pprint
+
+parser = CustomParsers()
+
+device_name="R1"
+
+output="""
+
+R1#  show service-routing database
+Service-Routing Database
+Service ID (Service:Subservice:Instance)         Trust     Domain Owner Size
+------------------------------------------------ --------- ------ ----- -----
+      100:1:31000000-0000-0000-0000-520000000000 Connected *      1       392
+      100:2:31000000-0000-0000-0000-520000000000 Connected *      1      1074
+      101:2:675952D8-B2F9-38A8-AE06-8CE5000160D1 Connected 1      2       565
+      101:2:933EF17C-54BA-5610-0325-B23200019D2E Connected 1      4       387
+      102:2:933EF17C-54BA-5610-0325-B23200019D2E Connected 1      4       387
+"""
+
+result=parser.showservdatabase(device_name=device_name,output=output,yaml_out=f'./show_service_routing_{device_name}.yaml')
+pprint.pprint(result, width=20)
+import pdb; pdb.set_trace()
