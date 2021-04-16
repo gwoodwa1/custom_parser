9b3abc143d3e25004ae9e3b91f521982b2d446b6
Gary Woodward <63735312+gwoodwa1@users.noreply.github.com>
2021-04-16 09:00:57 +0100

    Add files via upload

---
+++ testlogic.py
@@ -0,0 +1,32 @@
+from genie.utils import Dq
+import yaml
+import pprint
+
+device_name= 'R1'
+
+ 
+with open(f'./show_service_routing_{device_name}.yaml') as file:
+    output = yaml.load(file, Loader=yaml.FullLoader)
+ 
+
+
+failed_entries = Dq(output).contains('trust').not_contains('Connected').get_values('[3]')
+ 
+failed_dict={}
+ 
+for k in failed_entries:
+    temp_dict = failed_dict.setdefault('hostname',{}).setdefault(device_name,{}).setdefault('index',{}).setdefault(k, output['hostname'][device_name]['index'][k])
+
+
+pprint.pprint(failed_dict)   
+
+
+if failed_dict != {}:
+    with open(f'FAILED_show_service_{device_name}', 'w') as file:
+        yaml.dump(failed_dict, file, allow_unicode=True)
+      
+   
+ 
+ 
+ 
+ 
