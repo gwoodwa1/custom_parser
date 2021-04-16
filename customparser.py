6a86327b952db8584d9b2d3b93aaaa124fab9e76
Gary Woodward <63735312+gwoodwa1@users.noreply.github.com>
2021-04-16 11:23:01 +0100

    Update customparser.py
    Updated Formatting

--- customparser.py
+++ customparser.py
@@ -13,7 +13,6 @@ import yaml
 
 
 class CustomParsers:
-    
     def showservdatabase(self, output=None, device_name=None, yaml_out=None):
     
         result = output
@@ -41,35 +40,33 @@ class CustomParsers:
             if match_pattern:
                 index = index + 1
                 group = match_pattern.groupdict()
-                
+
                 temp_dict = (
                     self.output_dict.setdefault("hostname", {})
                     .setdefault(device_name, {})
                     .setdefault("index", {})
                     .setdefault(index, {})
                 )
-                if group['service'] and group['service'].isdigit():
+                if group["service"] and group["service"].isdigit():
                     temp_dict.update({"service_id": int(group["service"])})
-                if group['sub_service'] and group['service'].isdigit():
+                if group["sub_service"] and group["service"].isdigit():
                     temp_dict.update({"sub_service": int(group["sub_service"])})
-                
-                
+
                 temp_dict.update({"instance": group["instance"]})
                 temp_dict.update({"trust": group["trust"]})
                 temp_dict.update({"domain": group["domain"]})
-                if group['owner'] and group['owner'].isdigit():
+                if group["owner"] and group["owner"].isdigit():
                     temp_dict.update({"owner": int(group["owner"])})
-                if group['size'] and group['size'].isdigit():
-                   temp_dict.update({"size": int(group["size"])})
-                
+                if group["size"] and group["size"].isdigit():
+                    temp_dict.update({"size": int(group["size"])})
+
         if yaml_out:
-            
+
             CustomParsers().yamlwriter(yaml_out=yaml_out, input_dict=self.output_dict)
 
         return self.output_dict
 
     def yamlwriter(self, yaml_out=None, input_dict=None):
 
-        with open(f'{yaml_out}', "w") as file:
+        with open(f"{yaml_out}", "w") as file:
             yaml.dump(input_dict, file, allow_unicode=True)
-        
